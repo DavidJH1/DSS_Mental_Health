c27707df41c94ddd504077c29c2f40429e2b5069
DavidJH1 <han15038@byui.edu>
2023-09-27 14:09:40 -0600

    adding basic layout

--- ui.R
+++ ui.R
@@ -1,1 +1,14 @@
 #code for the user interface 
+library(shinydashboard)
+library(shiny)
+
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+server <- function(input, output, session) { }
+
+shinyApp(ui, server)
