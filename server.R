7e29d724e46fbe438d4b2267b83449651029056f
DavidJH1 <han15038@byui.edu>
2023-10-11 17:31:05 -0600

    Adding reactive filtering and graph

--- server.R
+++ server.R
@@ -2,6 +2,7 @@
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
+library(tidyverse)
 
 
 server <- function(input, output, session) {
@@ -13,5 +14,51 @@ server <- function(input, output, session) {
     
     }) 
 
+
+  #make a reactive statement that filters air quality with user selected values
+  final_data <- reactive({
+  
+  airquality2 <- airquality %>% mutate(Month_name = case_when(Month == 5 ~ "May",
+                                     Month == 6 ~ "June",
+                                     Month == 7 ~ "July",
+                                     Month == 8 ~ "August",
+                                     Month == 9 ~ "September"))
+    
+  
+  #filter by concrete values  
+  airquality_filtered <- airquality2 %>% filter(between(Ozone, input$ozone[1], input$ozone[2]),
+                                               Solar.R >= input$solar[1],
+                                               Solar.R <= input$solar[2],
+                                               Wind >= input$wind[1],
+                                               Wind <= input$wind[2],
+                                               Temp >= input$temp[1],
+                                               Temp <= input$temp[2],
+                                               between(Day, input$day[1], input$day[2]))
+  #filter by the month
+  if(input$month == "All") {
+    airquality_filtered <- airquality_filtered
+  }
+  
+  if(input$month != "All") {
+    airquality_filtered <- airquality_filtered %>% filter(Month_name == input$month)
+  }
+  
+  return(airquality_filtered)
+  
+  }) #end reactive filtering for air quality
+  
+  
+  #output interactive scatter plot
+  output$filtered_plot <- renderPlot({
+    ggplot(final_data(), aes_string(x = input$xvar, y = input$yvar)) +
+      geom_point()
+  })
+  
+  
+  output$airquality_data <- renderTable({
+    data.frame(final_data())
+  })
+  
+  
   }
 
--- ui.R
+++ ui.R
@@ -1,6 +1,8 @@
 #code for the user interface 
 library(shinydashboard)
 library(shiny)
+library(ggplot2)
+library(tidyverse)
 
 source("server.R")
 ui <- dashboardPage(
@@ -12,6 +14,7 @@ ui <- dashboardPage(
       id = "tabs",
     menuItem("Dashboard", tabName = "Dashboard"),
     menuItem("Patients", tabName = "Patients"),
+    menuItem("PatientsTest", tabName = "PatientsTest"),
     menuItem("Medication", tabName = "Medication"),
     menuItem("Condition", tabName = "Condition")
     
@@ -37,19 +40,44 @@ ui <- dashboardPage(
         ),
           column(8,
                  box(
-                   width = 8, status = "info", solidHeader = TRUE,
+                   status = "info", solidHeader = TRUE,
                    title = "Filter Options",
                    selectInput(inputId = "column1", choices = c("a","b","c"), label = "Alphabet"),
                    selectInput(inputId = "column2", choices = c("x","y","z"), label = "Polynomial")
                  )
                
         ))
-      ) # end tab Patients
+      ), # end tab Patients
+      tabItem("PatientsTest",
+        fluidRow(
+          column(4, 
+                 box(
+                   status = "info", solidHeader = TRUE,
+                   title = "Filter Options",
+                   sliderInput(inputId = "ozone", "Ozone Levels", 0, 100, value = c(0,100)),
+                   sliderInput(inputId = "solar", "Solar.R", 0, 400, value = c(0,400)),
+                   sliderInput(inputId = "wind", "Wind Speed", 0, 25, value = c(0,25)),
+                   sliderInput(inputId = "temp", "Temperature", 30, 100, value = c(30,100)),
+                   selectInput(inputId = "month", "Month", c("All","May","June","July", "August", "September")),
+                   sliderInput(inputId = "day", "Day", 1, 31, value = c(1,31)),
+                   selectInput(inputId = "xvar", "X-Axis", names(airquality), selected = "Ozone"),
+                   selectInput(inputId = "yvar", "Y-Axis", names(airquality), selected = "Solar.R")
+                 )
+                 
+                 ),
+          column(8, plotOutput(outputId = "filtered_plot")),
+          column(12, box(
+            status = "info", solidHeader = TRUE, title = "Data",
+            tableOutput(outputId = "airquality_data"),
+            
+            )
+          )
+        )
+      ) # endtab Paitent Test
     ) #end tabItems
     
   ) #end Dashboard body
 
 )# End Dashboard Page
 
-
-shinyApp(ui, server)
+shinyApp(ui, server, options = list(height = 1000))
